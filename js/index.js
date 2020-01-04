5cf70e36482c0334a5c0db791f2e315e11ccd688
Chris G <christophergray0709@gmail.com>
2020-01-04 02:08:57 -0800

    finishing-up-skills
    skills section WIP.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+function fade() {
+$('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 3000);
+
+$(document).ready(function(){
+  $(window).scroll(function() {
+      $("p").show().fadeOut("slow");
+  });
+});
+
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
 });
+
