3904c3bde4be431f5e18b45db082baf16fd41c5a
Chris G <christophergray0709@gmail.com>
2020-01-05 01:21:16 -0800

    Bootstrap_JS
    project complete.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,42 @@
-function fade() {
-$('.preloader').fadeOut("slow");
-}
-setTimeout(fade, 3000);
+//function fade() {
+//$('.preloader').fadeOut("slow");
+//}
+//setTimeout(fade, 3000);
 
-$(document).ready(function(){
-  $(window).scroll(function() {
-      $("p").show().fadeOut("slow");
-  });
-});
+//$(document).ready(function(){
+  //$(window).scroll(function() {
+    //  $("p").show().fadeOut("slow");
+//  });
+//});
 
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();   
 });
 
-ScrollReveal().reveal('.img-reveal');
+//ScrollReveal().reveal('.img-reveal', {delay: 500});
+
+
+//$('div.animalcontent').hide();
+//$('div').hide();
+//$('p.animal').bind('mouseover', function() {
+  //  $('div.animalcontent').fadeOut();
+    //$('#'+$(this).attr('id')+'content').fadeIn();
+//});  
+
+$('.alert').alert();
+
+//$(function(){
+  //  $("#myNav").linkUnderlineAnim();
+  //});
+
+
+//$("#myNav").linkUnderlineAnim({ 
+    // animation speed in milliseconds
+  //  "speed"       : 300,
+    // hex color
+ //   "color"       :"#28A745",
+    // thickness
+ //   "thickness"     : 2,
+    // distance in pixels from top
+ //   "distance"      : 0
+ // });
